102948de3a432db224701930dc001eb61c7a2a0a
Yongmin0423 <yym21345@gmail.com>
2024-12-28 04:18:51 +0900

    test4

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/crypto-tracker-practice/", // 서브디렉토리 경로로 설정
   plugins: [react()],
-})
+});
